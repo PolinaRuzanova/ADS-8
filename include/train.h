a8756cf8f97047cb6373e3757f8cf3fb856a0958
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-05-07 00:37:28 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -11,8 +11,10 @@ class Train {
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
   Cage *first; // точка входа в поезд (первый вагон)
+  int Tsize;
+  Cage* tail;
  public:
-  Train();
+  Train():first(nullptr), tail(nullptr), countOp(0) {};
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
